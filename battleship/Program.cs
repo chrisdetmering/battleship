380067c6d2811b3a117879375d2965a97e533726
BryanGK <bryangordonkrauss@gmail.com>
2021-03-14 09:51:37 -0700

    Getting started with C#

--- battleship/Program.cs
+++ battleship/Program.cs
@@ -7,6 +7,17 @@ namespace battleship
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int myInt = 10;
+            byte myByte = (byte)myInt;
+            double myDouble = (double)myByte;
+            string myString = "false";
+            myString = myInt.ToString();
+            short myShort = (short)myInt;
+            char myChar = 'x';
+            long myLong = (long)myInt;
+            decimal myDecimal = (decimal)myLong;
+            myString = myString + myInt + myByte + myDouble + myChar + myDecimal;
+            Console.WriteLine(myString);
         }
     }
 }
