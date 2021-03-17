f738eb3fdc8c037bffc47bd2601dc62d4bbcff6f
BryanGK <bryangordonkrauss@gmail.com>
2021-03-17 14:06:03 -0700

    Game functionality there for the most part

--- battleship/Battleship.cs
+++ battleship/Battleship.cs
@@ -4,50 +4,71 @@ namespace battleship
 {
     class Battleship
     {
-        private int[] location1 = new int[2];
-        private int[] location2 = new int[2];
-        private int[] location3 = new int[2];
+        Random random = new Random();
+        public int[] location1 = new int[2];
+        public int[] location2 = new int[2];
+        public int[] location3 = new int[2];
+        public int[] location4 = new int[2];
+        public int[] location5 = new int[2];
         private int shipDirectionX;
         private int shipDirectionY;
+        private int randomLocationX;
+        private int randomLocationY;
 
-        public int[] RandomShipLoc()
+        private int randomNumber(int max)
         {
-            Random random = new Random();
+            var randomLocation = random.Next(1, max);
+            return randomLocation;
+        }
 
+        public void RandomShipLoc()
+        {
             var shipDirectionRandomizer = random.Next(0, 2);
+
             if (shipDirectionRandomizer == 0)
             {
                 shipDirectionX = 0;
                 shipDirectionY = 1;
+                randomLocationX = randomNumber(11);
+                randomLocationY = randomNumber(6);
             }
             else
             {
                 shipDirectionX = 1;
                 shipDirectionY = 0;
+                randomLocationX = randomNumber(6);
+                randomLocationY = randomNumber(11);
             }
-            Console.WriteLine(shipDirectionRandomizer);
-
-            var randomLocationX = random.Next(1, 11);
-            var randomLocationY = random.Next(1, 11);
 
             location1.SetValue(randomLocationX, 0);
             location1.SetValue(randomLocationY, 1);
+
             location2.SetValue(randomLocationX + shipDirectionX, 0);
             location2.SetValue(randomLocationY + shipDirectionY, 1);
+
             location3.SetValue(randomLocationX + shipDirectionX * 2, 0);
             location3.SetValue(randomLocationY + shipDirectionY * 2, 1);
 
-            Console.Write(location1[0] + ", ");
-            Console.WriteLine(location1[1]);
-            Console.Write(location2[0] + ", ");
-            Console.WriteLine(location2[1]);
-            Console.Write(location3[0] + ", ");
-            Console.WriteLine(location3[1]);
+            location4.SetValue(randomLocationX + shipDirectionX * 3, 0);
+            location4.SetValue(randomLocationY + shipDirectionY * 3, 1);
 
-            //random number for vertical or horizontal
-            //random number 0-9 + 1 for location1
-            //add values to make loc2 and loc3 adjascent by vertical or horizontal.
-            return location1;
+            location5.SetValue(randomLocationX + shipDirectionX * 4, 0);
+            location5.SetValue(randomLocationY + shipDirectionY * 4, 1);
+
+            //Console.WriteLine(shipDirectionRandomizer);
+            //Console.WriteLine($"X LOC:{randomLocationX}");
+            //Console.WriteLine($"Y LOC:{randomLocationY}");
+
+            //Console.Write(location1[0] + ", ");
+            //Console.WriteLine(location1[1]);
+            //Console.Write(location2[0] + ", ");
+            //Console.WriteLine(location2[1]);
+            //Console.Write(location3[0] + ", ");
+            //Console.WriteLine(location3[1]);
+            //Console.Write(location4[0] + ", ");
+            //Console.WriteLine(location4[1]);
+            //Console.Write(location5[0] + ", ");
+            //Console.WriteLine(location5[1]);
         }
     }
 }
--- battleship/Program.cs
+++ battleship/Program.cs
@@ -7,23 +7,23 @@ namespace battleship
     {
         public static void Main(string[] args)
         {
-            Random random = new Random();
-
             GameBoard gameBoard = new GameBoard();
             Player player = new Player();
             Battleship battleship = new Battleship();
             Console.Clear();
-            Console.WriteLine("Welcome to C# console Battleship.\n \n");
+            Console.WriteLine("\t\t\tWelcome to C# console Battleship.\n \n");
 
             gameBoard.drawGameBoard();
 
             battleship.RandomShipLoc();
 
-            while ((player.shots < player.maxShots) && (player.hits < 3))
+            Console.WriteLine("Shots remaining: " + (player.maxShots - player.shots) + "\n\n");
+
+            while ((player.shots < player.maxShots) && (player.hits < 5))
             {
-                Console.Write("Please enter X coordinate from 1-10: ");
+                Console.Write("Please enter X horizontal coordinate from 1-10: ");
                 int guessX = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Please enter Y coordinate from 1-10: ");
+                Console.Write("Please enter Y vertical coordinate from 1-10: ");
                 int guessY = Convert.ToInt32(Console.ReadLine());
 
                 if ((guessX < 1 || guessX > 10) || (guessY < 1 || guessY > 10))
@@ -32,19 +32,39 @@ namespace battleship
                 }
                 else
                 {
-
-                    gameBoard.gameBoardArr[guessY, guessX] = " X ";
-                    Console.Clear();
-                    Console.WriteLine("Welcome to C# console Battleship.\n \n");
-
+                    if ((guessX == battleship.location1[0]
+                        || guessX == battleship.location2[0]
+                        || guessX == battleship.location3[0]
+                        || guessX == battleship.location4[0]
+                        || guessX == battleship.location5[0])
+                        && (guessY == battleship.location1[1]
+                        || guessY == battleship.location2[1]
+                        || guessY == battleship.location3[1]
+                        || guessY == battleship.location4[1]
+                        || guessY == battleship.location5[1]))
+                    {
+                        gameBoard.gameBoardArr[guessY, guessX] = ">X<";
+                        player.hits++;
+                        player.shots++;
+                        Console.Clear();
+                        Console.WriteLine("\t\t\t\t\t--> HIT! <--\n\n");
+                    }
+                    else
+                    {
+                        gameBoard.gameBoardArr[guessY, guessX] = "> <";
+                        player.shots++;
+                        Console.Clear();
+                        Console.WriteLine("\t\t\t\t\t--> MISS! <--\n\n");
+                    }
+                    
                     gameBoard.drawGameBoard();
 
-                    player.shots++;
-                    Console.WriteLine("Shots remaining: " + (player.maxShots - player.shots));
+                    Console.WriteLine("Shots remaining: " + (player.maxShots - player.shots) + "\n\n");
+                    
                 }
             }
-            //int zeroToNine = random.Next(10);
-            //Console.WriteLine(zeroToNine);
+            if (player.hits == 5) Console.WriteLine("\t\t\t\tYou sunk my Battleship!!");
+            else Console.WriteLine("\t\t\t\tThey got away!");
         }
     }
 }
